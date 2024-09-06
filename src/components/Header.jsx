7ce11a7bae53cadf2857a35d8d69e005c7962dda
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-09-06 15:53:32 -0500

    created Header component and completed company logo with name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,18 @@ const Header = () => {
   const [openNavigation, setOpenNavigation] = useState(false);
 
   return (
-    <div className={`fixed top-0 left-0 w-full z-50 border-b border-neutral-800 lg:backdrop-blur-sm`}>
-      <div className="flex items-center py-4 px-5 lg:px-8 xl:px-10">
+    <div className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"}`}>
+      <div className="flex items-center py-4 px-5 xl:px-12">
 
-        <a href="#hero" className="block w-[12rem] xl:mr-8">
-          <img src={companyLogo} width={80} height={40} alt="Barbell Logo" className="rounded-full"/>
+        <a href="#hero" className="flex w-[12rem] xl:mr-8 gap-5">
+          <img src={companyLogo} width={60} height={20} alt="Barbell Logo" className="rounded-full"/>
+          <h1 className="text-[1.5rem] relative top-4"><nobr>Barbell Fitness</nobr></h1>
         </a>
 
-        <nav>
-          
+        <nav className={`${openNavigation ? "flex" : "hidden"} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            
+          </div>
         </nav>
 
       </div>
