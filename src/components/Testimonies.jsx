b32fcb6ee82f84f556b219e4570cb3df15af8475
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-09-12 21:10:15 -0500

    fixed positioning of 'learn more >' div with styling color and hover effect and wrapped by Link component for future link

--- src/components/Testimonies.jsx
+++ src/components/Testimonies.jsx
@@ -9,6 +9,7 @@ import { testimonies } from "../constants";
 import { words } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import { Link } from "react-router-dom";
 
 const Testimonies = () => {
 
@@ -24,23 +25,26 @@ const Testimonies = () => {
           {/* UP to xl screen size it's a carousel */}
           <Carousel slides={testimonies} slidesPerView className="xl:hidden translate-y-10"/>
 
-          <ScrollParallax>
-            <div className="hidden xl:flex justify-between flex-wrap gap-15 translate-y-[4rem]" ref={parallaxref}>
-                {testimonies.map((item)=>{
-                  return(
-                    <iframe key={item.id} src={item.url} width="500" height="265" style={{border:"none", overflow:"hidden", borderRadius: "10px"}}  frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" 
-                    className={`pointer-events-none max-w-full ${item.styles ? item.styles : ""}`}/>
-                  )
-                })}
+          <div className="flex flex-col items-center">
+            <ScrollParallax>
+              <div className="hidden xl:flex justify-between flex-wrap gap-15 translate-y-[4rem]" ref={parallaxref}>
+                  {testimonies.map((item)=>{
+                    return(
+                      <iframe key={item.id} src={item.url} width="500" height="265" style={{border:"none", overflow:"hidden", borderRadius: "10px"}}  frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" 
+                      className={`pointer-events-none max-w-full ${item.styles ? item.styles : ""}`}/>
+                    )
+                  })}
+              </div>         
+            </ScrollParallax>
+
+            <Link to="/success-stories">
+              <div className="flex translate-y-10 hover:underline decoration-n-8">            
+                <h6 className="h6 uppercase">Learn <span style={{color: "#0E0C15"}}>More</span></h6>             
+                <Arrow />
               </div>
-          </ScrollParallax>
-
-          {/* WORK on positioning and styling this div which will be a link to another component with more reviews/success stories */}
-          <div className="flex">
-            <h1 className="">More</h1>
-            <Arrow />
+            </Link>
           </div>
-
+             
         </div>
     </Section>
   )
