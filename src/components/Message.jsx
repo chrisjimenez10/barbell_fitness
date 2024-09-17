f4f362950f7258e05f39566ee09af2cd1da954a5
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-09-17 12:14:27 -0500

    completed emailjs feature to send email to owner from website user with input validation and restriction using regext pattern

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,5 @@
 //Imports
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
   //Here, we are using EmailJS service to send client-side emails --> NOTE: emailjs has three methods and we are using sendForm() --> We can also use send(), but we need to pass the individual Template Parameters that match what we have in our Template at the EmailJS Website as a JSON Object (both key-values wrapped in quotes)
 import emailjs from "@emailjs/browser";
 
@@ -7,13 +7,32 @@ const Message = () => {
 
   //We can pass the "ref" attribute to our <form> element, so we escape the need to have to update and store input values through state --> This will interact directly with the DOM and all the input values will be stored within the variable we assign the "ref" attribute to, so for us the "form" variable --> We can then use the sendForm() method of emailjs and pass the Template Parameters via "form.current"
   const form = useRef();
+  const phoneInput = useRef();
+
+  useEffect(()=>{
+    const inputElement = phoneInput.current;
+    inputElement.addEventListener("input", (e)=>{
+      //Regext Pattern for phone_number input field
+      const regex = /^[0-9]*$/; //Accept ONLY Digits 0-9
+      if(!regex.test(e.target.value)){
+        //Here, we are slicing the value that is provided by the user that DOES NOT pass the regex.test() --> The input is a string, so the index position -1 refers to the LAST/MOST RECENTLY APPENDED element of the string 
+        e.target.value = e.target.value.slice(0, -1);
+      }
+    })
+  },[])
 
   //Functions
   const sendEmail = async (e) => {
     e.preventDefault();
-    await emailjs.sendForm("service_c5j2uhf", "template_g9ftjqm", form.current, {
-      publicKey: "IuG3Dwb1SMJ2tutmX",
-    });
+    if(!form.current) return; //Check for null
+
+    try{
+      await emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, {
+        publicKey: import.meta.env.VITE_PUBLIC_KEY,
+      });
+    }catch(error){
+      console.error("Error sending email:", error);
+    }
   };
 
 
@@ -29,7 +48,7 @@ const Message = () => {
         <input id="user_email" name="user_email" type="email" required/>
 
         <label htmlFor="phone_number">Phone Number: </label>
-        <input id="phone_number" name="phone_number" type="text"/>
+        <input id="phone_number" name="phone_number" type="tel" ref={phoneInput}/>
 
         <label htmlFor="message">Message: </label>
         <textarea id="message" name="message" required/>
